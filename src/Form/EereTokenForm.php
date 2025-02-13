f3e6a46df5836f51948af6e53db4c6d8cf52104d
KO <kovezberde@gmail.com>
2025-02-13 11:20:13 -0500

    Update EereTokenForm.php

--- src/Form/EereTokenForm.php
+++ src/Form/EereTokenForm.php
@@ -38,13 +38,7 @@ class EereTokenForm extends FormBase
     if (is_int($form_session)) {
 
       $form['doe'] = array(
-        '#markup' => '<div style="background-color: #a6dc4833; padding: 15px; border: 1px solid #ddd; width: 62%">'
-          . 'This is a Department of Energy (DOE) computer system. DOE computer systems are provided for the processing of official U.S. government information only. '
-          . 'All data contained within DOE computer systems is owned by the DOE, and may be audited, intercepted, recorded, read, copied, or captured in any manner '
-          . 'and disclosed in any manner, by authorized personnel. THERE IS NO RIGHT OF PRIVACY IN THIS SYSTEM. '
-          . 'System personnel may disclose any potential evidence of crime found on DOE computer systems to appropriate authorities. '
-          . 'USE OF THIS SYSTEM BY ANY USER, AUTHORIZED OR UNAUTHORIZED, CONSTITUTES CONSENT TO THIS AUDITING, INTERCEPTION, RECORDING, READING, COPYING, CAPTURING, '
-          . 'and DISCLOSURE OF COMPUTER ACTIVITY.</div>',
+        '#markup' => '<div style="background-color: #a6dc4833; padding: 15px; border: 1px solid #ddd; width: 62%"></div>',
       );
 
       $form['token'] = array(
